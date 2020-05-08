3c78fafbaf71de64f75622af909e49c37b0276cf
Nazar Pyhlyuk <@gmail.com>
2020-05-08 17:53:29 +0300

    Updates

---
+++ precache-manifest.098e9eca4c41f2b793d3f13291c7337a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5fa31c4b81d5197e9f21bd71049baa",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "a9aab121ce9730331e79",
+    "url": "/weatherapp/static/css/main.b5776238.chunk.css"
+  },
+  {
+    "revision": "6e4d507214af9f0271fe",
+    "url": "/weatherapp/static/js/2.a7136f74.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/weatherapp/static/js/2.a7136f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9aab121ce9730331e79",
+    "url": "/weatherapp/static/js/main.a0b08267.chunk.js"
+  },
+  {
+    "revision": "37a461f81c69d6959f2d",
+    "url": "/weatherapp/static/js/runtime-main.5392d643.js"
+  },
+  {
+    "revision": "37484ed8e2f409ab77c8999c94da4c04",
+    "url": "/weatherapp/static/media/arrowdown.37484ed8.png"
+  },
+  {
+    "revision": "67350507344d8cfa5f2cf68b93acc150",
+    "url": "/weatherapp/static/media/arrowup.67350507.png"
+  },
+  {
+    "revision": "84d33f183b298b7bfda2a4426ffb0a7a",
+    "url": "/weatherapp/static/media/bg22.84d33f18.jpg"
+  },
+  {
+    "revision": "e3802a712dfead700776e24a239f9d35",
+    "url": "/weatherapp/static/media/logo1.e3802a71.PNG"
+  },
+  {
+    "revision": "773e9c96302d513491ca03bf33dd558a",
+    "url": "/weatherapp/static/media/n2.773e9c96.jpg"
+  },
+  {
+    "revision": "51d951a3827e1d4f976cab4cdf996673",
+    "url": "/weatherapp/static/media/search.51d951a3.png"
+  }
+]);
